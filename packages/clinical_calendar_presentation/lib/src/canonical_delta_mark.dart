43632d489480af15eee8442ae5cfbe2b58d44449
mshamblin5150-code <295951288+mshamblin5150-code@users.noreply.github.com>
2026-08-12 17:53:50 -0400

    Revert "perf(themes): decode shared mark at display size"
    This reverts commit 71e8a9e73b0efc5de9aa16bd882d12057c76d818.

--- packages/clinical_calendar_presentation/lib/src/canonical_delta_mark.dart
+++ packages/clinical_calendar_presentation/lib/src/canonical_delta_mark.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 const canonicalDeltaMarkAsset = 'assets/shared_brand/axion-delta-mark.png';
@@ -15,7 +14,6 @@ final class CanonicalDeltaMark extends StatelessWidget {
     this.semanticLabel,
     this.imageKey,
     this.errorBuilder,
-    this.resizeForRuntime = !kDebugMode,
     super.key,
   });
 
@@ -25,35 +23,19 @@ final class CanonicalDeltaMark extends StatelessWidget {
   final String? semanticLabel;
   final Key? imageKey;
   final ImageErrorWidgetBuilder? errorBuilder;
-  @visibleForTesting
-  final bool resizeForRuntime;
 
   @override
-  Widget build(BuildContext context) => LayoutBuilder(
-    builder: (context, constraints) {
-      final logicalWidth =
-          size ?? (constraints.hasBoundedWidth ? constraints.maxWidth : null);
-      final cacheWidth =
-          resizeForRuntime &&
-              logicalWidth != null &&
-              logicalWidth.isFinite &&
-              logicalWidth > 0
-          ? (logicalWidth * MediaQuery.devicePixelRatioOf(context)).ceil()
-          : null;
-      return Image.asset(
-        canonicalDeltaMarkAsset,
-        key: imageKey,
-        package: 'clinical_calendar_presentation',
-        width: size,
-        height: size,
-        cacheWidth: cacheWidth,
-        fit: BoxFit.contain,
-        filterQuality: FilterQuality.high,
-        color: color,
-        colorBlendMode: colorBlendMode,
-        semanticLabel: semanticLabel,
-        errorBuilder: errorBuilder,
-      );
-    },
+  Widget build(BuildContext context) => Image.asset(
+    canonicalDeltaMarkAsset,
+    key: imageKey,
+    package: 'clinical_calendar_presentation',
+    width: size,
+    height: size,
+    fit: BoxFit.contain,
+    filterQuality: FilterQuality.high,
+    color: color,
+    colorBlendMode: colorBlendMode,
+    semanticLabel: semanticLabel,
+    errorBuilder: errorBuilder,
   );
 }
